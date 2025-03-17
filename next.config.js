e86196efeb11b3a6fb55a536fb3be4c937794efe
govispire <govimg21@gmail.com>
2025-03-17 11:10:05 +0000

    Go

--- next.config.js
+++ next.config.js
@@ -13,8 +13,8 @@ const nextConfig = {
 
 if (process.env.NEXT_PUBLIC_TEMPO) {
   nextConfig["experimental"] = {
-    // NextJS 13.4.8 up to 14.1.3:
-    swcPlugins: [[require.resolve("tempo-devtools/swc/0.86"), {}]],
+    // Using compatible SWC plugin version for Next.js 15+
+    swcPlugins: [[require.resolve("tempo-devtools/swc/0.90"), {}]],
   };
 }
 
